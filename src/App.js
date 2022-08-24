fe19588149f4dd55171ea233add90218885ea7b4
Connor Cyphers <79062185+connor-cs@users.noreply.github.com>
2022-08-23 22:53:21 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Form from "./Components/Form";
 function App() {
 
   const [tasks, setTasks] = useState([])
-  const [editTask, seteditTask] = useState({})
+  const [editTask, setEditTask] = useState({})
 
   //GET
   const fetchData = () => {
@@ -17,9 +17,7 @@ function App() {
       setTasks([...dataAPI])
     })
   }
-  //clear edit task obj
-  //update state to reflect new changes in form
-  //send patch
+  
   useEffect(() => {
     fetchData()
   }, [])
@@ -47,28 +45,12 @@ function App() {
     .then(() => setTasks(tasks.filter(task=>task.id !== id)))
   }
 
-  //PATCH
-  // function handleEdit(id, update){
-  //   fetch(`http://localhost:9292/todos/${id}`, {
-  //     method: "PATCH",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //     body: JSON.stringify({
-  //       taskname: update
-  //     }),
-  //   })
-  //   .then(r=>r.json())
-  //   .then(() => setTasks(tasks))
-  // }
-
-
 
   return(
     <div className="App">
       <Header />
       <Form handleAdd={handleAdd} />
-      <TaskContainer seteditTask={seteditTask} tasks={tasks} handleDelete={handleDelete} />
+      <TaskContainer setEditTask={setEditTask} tasks={tasks} handleDelete={handleDelete} />
     </div>
 )}
 
